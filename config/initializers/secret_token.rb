a1a1de385ff86b03582b2ed9e74c1220adf4a21e
Mike Michaelevich <mmichael0413@gmail.com>
2012-07-28 11:31:30 -0400

    add it all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roqem::Application.config.secret_token = 'f396e282f354d36172817431e0e591f9bcff8b4b69b36f7cc487950e586da0ad2eb465ef462292b68edfdfda59b0485057b7983ae2da19d88d28b56e52cd25ee'
