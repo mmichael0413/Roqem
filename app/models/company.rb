6608c310540ef2244792ab5faad1b95bb41a84bf
Mike Michaelevich <mmichael0413@gmail.com>
2012-07-30 21:03:36 -0400

    update api function to receive company and post tokens and create new company and post based on params

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,3 @@
 class Company < ActiveRecord::Base
   has_many :posts
-  
-  def self.new_company(company_id)
-    company = Company.new
-    company_token = Company.company_id
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
   belongs_to :company
+  
+  def self.new_post(company, token)
+    comp = Company.find_or_create_by_company_token(company)
+    
+    post = Post.new
+    post.post_token = token
+    post.company_id = comp.id
+    post.title = "YoMama"
+    post.body = "Bo body, bo body. B. Business. Biz. Niz."
+    post.save
+  end
 end
