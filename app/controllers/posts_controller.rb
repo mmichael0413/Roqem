40a6b5547d108ad9623bbf8262e09711cf01e6e8
Mike Michaelevich <mmichael0413@gmail.com>
2012-07-30 21:10:58 -0400

    add information to post index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   
   def create
@@ -8,8 +9,8 @@ class PostsController < ApplicationController
   #Function that grabs the company id and title token from CedarStack and
   #creates a new company (if one doesn't already exist with the id)
   def api
-    if params[:company_id, :post_token]
-      Post.new_post(params[:company_id, :post_token])
+    if params[:company_id] && params[:post_token]
+      Post.new_post(params[:company_id], params[:post_token])
     end
   end
 end
